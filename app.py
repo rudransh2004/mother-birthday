2d635cd332b8d08a1f9027200faf17ba0b6d3caf
RUDRANSH AGNIHOTRI <59211507+rudransh2004@users.noreply.github.com>
2020-02-10 20:07:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
